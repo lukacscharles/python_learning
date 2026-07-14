e5e3f1a96bf4a7bf3d18c6b879ed884aa9805956
lukacscharles <lukacs.charles@gmail.com>
2026-07-14 21:15:53 +0200

    second homework

---
+++ masodik_valtozok/friends.py
@@ -0,0 +1,13 @@
+"""✍️ IV. HÁZI FELADAT
+Állíts be három nevet egy változó értékének, legyen Katalin, András és Zsanett.
+A hozzárendelt változó neve legyen my_friends (barátaim).
+Írasd ki a konzolba a változó értékét.
+Cseréld ki az 1.pontban ismertetett változó értékeit az alábbi nevekre: Anita, István, Gábor
+Futtasd a programot a változtatások után!"""
+
+
+friends="Katalin, András, ZSanett"
+print(friends)
+friends="Anita, István, Gábor"
+print(friends)
+input() 
