14b3204230528d0163294609c16835406e13aafd
lukacscharles <lukacs.charles@gmail.com>
2026-07-19 20:37:09 +0200

    version 2 of 2 of 4

---
+++ negyedik_szamok/version2_of2.py
@@ -0,0 +1,18 @@
+""" Változók létrehozása """
+
+customer_1_name = "András"  # Név
+customer_1_age = 39   # Életkor
+
+customer_2_name = "Judit"  # Név
+customer_2_age = 42  # Életkor
+
+customer_3_name = "Gery"  # Név
+customer_3_age = 24  # Életkor
+
+
+""" Szöveges karakterlánc (string) kombinálása szám (integer) értékkel """
+
+# "customer_1", "customer_2", "customer_3" nevének és életkorának kiíratása
+print("Boldog" + " " + str(customer_1_age) + ". születésnapot kívánunk" + ", " + customer_1_name + "!")
+print("Isten éltessen" + " " + customer_2_name + ", " + str(customer_2_age) + ". születésnapod alkalmából szeretettel gratulálunk!")
+print("Kedves " + customer_3_name + "! Ma lettél " + str(customer_3_age) + " éves, Boldog szülinapot kívánunk a csapatunk nevében!")
